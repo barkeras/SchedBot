68ecaf25ecf211bb993d07a4ab957eba234964bc
barkeras <dayspringbroncs@gmail.com>
2016-11-09 16:07:09 -0500

    Initial Dashboard and Login Pages built

---
+++ SchedBot/Controllers/AccountController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SchedBot.Controllers
+{
+    public class AccountController : Controller
+    {
+        // GET: Account
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        [AllowAnonymous]
+        public ActionResult Login()
+        {
+            if (Request.IsAuthenticated)
+            {
+                return RedirectToAction("AccessDenied", "Error");
+            }
+
+            return View();
+        }
+    }
+}
---
+++ SchedBot/Models/Account/LoginUserViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SchedBot.Models.Account
+{
+    public class LoginUserViewModel
+    {
+        public String EmailAddress { get; set; }
+        public String Password { get; set; }
+        public bool RememberMe { get; set; }
+    }
+}
